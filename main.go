7864390c7e1bcadaab2da178ba9064bc8b89f833
Vadym Abramchuk <vadym@dev-branch.com>
2021-07-30 19:53:53 +0300

    refactor: move wsterm out of main module

--- main.go
+++ main.go
@@ -4,20 +4,19 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	tsize "github.com/kopoli/go-terminal-size"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
 
-	"gopkg.in/alecthomas/kingpin.v2"
-
-	"golang.org/x/crypto/ssh/terminal"
-
 	"github.com/gorilla/websocket"
+	tsize "github.com/kopoli/go-terminal-size"
 	"github.com/minio/pkg/wildcard"
 	"github.com/spf13/viper"
+	"gopkg.in/alecthomas/kingpin.v2"
+
+	"github.com/devbranch-vadym/portainerssh/wsterm"
 
 	_ "embed"
 )
@@ -83,81 +82,9 @@ type ContainerExecParams struct {
 	User    string
 }
 
-type WebTerm struct {
-	SocketConn *websocket.Conn
-	ttyState   *terminal.State
-	errChn     chan error
-}
-
 type TerminalDimensions struct {
 	Width  int
 	Height int
-}
-
-func (w *WebTerm) wsWrite() {
-	var err error
-	var keybuf [1]byte
-	for {
-		_, err = os.Stdin.Read(keybuf[0:1])
-		if err != nil {
-			w.errChn <- err
-			return
-		}
-
-		err = w.SocketConn.WriteMessage(websocket.BinaryMessage, keybuf[0:1])
-		if err != nil {
-			if websocket.IsCloseError(err, websocket.CloseNormalClosure) {
-				w.errChn <- nil
-			} else {
-				w.errChn <- err
-			}
-			return
-		}
-	}
-}
-
-func (w *WebTerm) wsRead() {
-	var err error
-	var raw []byte
-	for {
-		_, raw, err = w.SocketConn.ReadMessage()
-		if err != nil {
-			if websocket.IsCloseError(err, websocket.CloseNormalClosure) {
-				w.errChn <- nil
-			} else {
-				w.errChn <- err
-			}
-			return
-		}
-		os.Stdout.Write(raw)
-	}
-}
-
-func (w *WebTerm) SetRawtty(isRaw bool) {
-	if isRaw {
-		state, err := terminal.MakeRaw(int(os.Stdin.Fd()))
-		if err != nil {
-			panic(err)
-		}
-		w.ttyState = state
-	} else {
-		terminal.Restore(int(os.Stdin.Fd()), w.ttyState)
-	}
-}
-
-func (w *WebTerm) Run() {
-	w.errChn = make(chan error)
-	w.SetRawtty(true)
-
-	go w.wsRead()
-	go w.wsWrite()
-
-	err := <-w.errChn
-	w.SetRawtty(false)
-
-	if err != nil {
-		panic(err)
-	}
 }
 
 func (r *PortainerAPI) formatHttpApiUrl() string {
@@ -428,7 +355,7 @@ func main() {
 	}
 	conn := portainer.GetContainerConn(params)
 
-	wt := WebTerm{
+	wt := wsterm.WebTerm{
 		SocketConn: conn,
 	}
 	wt.Run()
